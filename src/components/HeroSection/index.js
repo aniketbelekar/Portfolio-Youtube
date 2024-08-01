2470b6c9d1bcf7c0d92c882d7441c338f9d3d903
Aniket  Belekar <abelekar18@gmail.com>
2024-08-01 20:42:46 +0530

    resume download button

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import HeroBgAnimation from '../HeroBgAnimation'
-import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
-import HeroImg from '../../images/HeroImage.jpg'
+import React from 'react';
+import HeroBgAnimation from '../HeroBgAnimation';
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle, ResumeButton } from './HeroStyle';
+import HeroImg from '../../images/HeroImage.jpg';
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
@@ -12,7 +12,7 @@ const HeroSection = () => {
                 <HeroBg>
                     <HeroBgAnimation />
                 </HeroBg>
-                <HeroInnerContainer >
+                <HeroInnerContainer>
                     <HeroLeftContainer id="Left">
                         <Title>Hi, I am <br /> {Bio.name}</Title>
                         <TextLoop>
@@ -28,18 +28,22 @@ const HeroSection = () => {
                             </Span>
                         </TextLoop>
                         <SubTitle>{Bio.description}</SubTitle>
-                        <ResumeButton href={Bio.resume} target='display'>Check Resume</ResumeButton>
+                        <ResumeButton
+                            href="/Aniket_resume_bpo_and_webpdf_1234.pdf" // Relative path to the PDF in the public folder
+                            target="_blank"
+                            download="Aniket_Resume.pdf" // Optional: specify a default filename for download
+                        >
+                            Check Resume
+                        </ResumeButton>
                     </HeroLeftContainer>
 
                     <HeroRightContainer id="Right">
-
                         <Img src={HeroImg} alt="hero-image" />
                     </HeroRightContainer>
                 </HeroInnerContainer>
-
             </HeroContainer>
         </div>
-    )
+    );
 }
 
-export default HeroSection
+export default HeroSection;
